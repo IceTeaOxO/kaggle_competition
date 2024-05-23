a5202664ef90e99d73719916ce8d6f3f60eeb4bf
henryhuang <uchuang9128@gmail.com>
2024-05-24 02:28:19 +0800

    update

--- Banking Apps Reviews Classification/BERT.py
+++ Banking Apps Reviews Classification/BERT.py
@@ -57,7 +57,7 @@ val_dataset = CustomDataset(val_texts, val_labels, tokenizer, max_length)
 # 設置訓練參數，包括 Early Stopping
 training_args = TrainingArguments(
     output_dir="./results",
-    num_train_epochs=2,
+    num_train_epochs=4,
     per_device_train_batch_size=8,
     per_device_eval_batch_size=8,
     warmup_steps=500,
@@ -105,8 +105,8 @@ pred_stars = [str(round(pred + 1)) + ' 顆星' for pred in predictions]
 
 # 儲存推論結果
 result_df = pd.DataFrame({"index": test_data["index"], "pred": pred_stars})
-result_df.to_csv("inference_result_BERT_chinese_v7.csv", index=False)
+result_df.to_csv("inference_result_BERT_chinese_v7_v2.csv", index=False)
 
 # 儲存訓練後的模型
-model_path = "./model/saved_model_BERT_chinese_v7"  # 指定模型的儲存路徑
+model_path = "./model/saved_model_BERT_chinese_v7_v2"  # 指定模型的儲存路徑
 trainer.save_model(model_path)
